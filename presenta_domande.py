baddc0b4f46394074578a7eddf9be9846ef96c8a
mattcar-ux <matteo.cardi@hotmail.it>
2024-11-29 19:59:38 +0100

    aggiornato domanda

--- presenta_domande.py
+++ presenta_domande.py
@@ -1,14 +1,13 @@
-def mostra_domande(domande, risposte):
-    import random
-    for num_dom in range(3)
-        print(f"Domanda: {domande[num_dom]}")
-        for num_risp in range(1, 4):
-            print(f"{num_risp}. {risposte[num_risp-1]}")
+def mostra_domande(domanda: str, risposte: list):
+    print(f"Domanda: {domanda}")
+    for num_risp in range(1, len(risposte)+1):
+        print(f"{num_risp}. {risposte[num_risp-1]}")
     input(f"Scegli una risposta indicando il numero corrispondente: ")
 
 domande = ["Cos'è la CPU?", "Cosa significa RAM?", "Cos'è un bit?"]
 
-risposta_1 = ["Central Processing Unit", "Computer Processors United", "Company Planning UK",
-            "Random Access Memory", "R a m", "R A M"]
+risposta_1 = ["Central Processing Unit", "Computer Processors United", "Company Planning UK"]
+risposta_2 = ["Random Access Memory", "Return And Mix", "Rage Against Machines"]
 
-mostra_domande(domande, risposta_1)
+mostra_domande(domande[0], risposta_1)
+mostra_domande(domande[1], risposta_2)
