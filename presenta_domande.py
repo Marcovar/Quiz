11f528fd8ae1d26eee55a7d7b873e42094e4a216
mattcar-ux <matteo.cardi@hotmail.it>
2024-11-29 20:08:18 +0100

    aggiunti messaggi per risposta corretta/sbagliata

--- presenta_domande.py
+++ presenta_domande.py
@@ -2,7 +2,11 @@ def mostra_domande(domanda: str, risposte: list):
     print(f"Domanda: {domanda}")
     for num_risp in range(1, len(risposte)+1):
         print(f"{num_risp}. {risposte[num_risp-1]}")
-    input(f"Scegli una risposta indicando il numero corrispondente: ")
+    risp_utente = input(f"Scegli una risposta indicando il numero corrispondente: ")
+    if risp_utente == "1":
+        print("Complimenti, risposta corretta!")
+    else:
+        print(f"Risposta errata: la risposta corretta era {risposte[0]}")
 
 domande = ["Cos'è la CPU?", "Cosa significa RAM?", "Cos'è un bit?"]
 
