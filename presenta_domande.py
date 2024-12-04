8b63dbf32e1bc16997f5a5d1a859b4a1ccdd8ee9
mattcar-ux <matteo.cardi@hotmail.it>
2024-12-04 21:47:37 +0100

    trasformato dict risposte in lista

--- presenta_domande.py
+++ presenta_domande.py
@@ -1,39 +1,36 @@
-def mostra_domande(domanda: str, risposte: dict):
+def mostra_domande(domanda: str, risposte: list):
     print(f"Domanda: {domanda}")
     i = 1
     x = 0
     # SHUFFLE
-    for risposta in risposte.values():
-        for option, value in risposta.items():
-            print(f"{i}. {option}")
-            if value == True:
-                x = i
-            i += 1
+    for option, value in risposte.items():
+        print(f"{i}. {option}")
+        if value == True:
+            x = i
+        i += 1
     risp_utente = input(f"Scegli una risposta indicando il numero corrispondente: ")
-    if risp_utente == x:
-        print("Complimenti, risposta corretta!")
+    if int(risp_utente) == x:
+        print("Complimenti, risposta corretta!\n")
     else:
-        print(f"Risposta errata") # DA FARE : la risposta corretta era {risposte.get(True)}
+        print(f"Risposta errata\n") # DA FARE : la risposta corretta era {risposte.get(True)}
 
 domande = ["Cos'è la CPU?",
            "Cosa significa RAM?",
            "Cos'è un bit?"]
-risp = {
-"risposta_1":{"Central Processing Unit": True,
-              "Computer Processors United": False,
-              "Company Planning UK": False},
-}
-
-# risposta_2:["Random Access Memory",
-#               "Return And Mix",
-#               "Rage Against Machines"]
-# risposta_3 = ["Un'unità di memoria",
-#               "Un simbolo nel codice",
-#               "Un elemento grafico"]
-# }
-risposta_giusta_1 = "Central Processing Unit"
-risposta_giusta_2 = "Random Access Memory"
-risposta_giusta_3 = "Un'unità di memoria"
+risp = [
+        {"Central Processing Unit": True,
+        "Computer Processors United": False,
+        "Company Planning UK": False},
+        {"Random Access Memory": True,
+        "Return And Mix": False,
+        "Rage Against Machines": False},
+        {"Un'unità di memoria": True,
+        "Un simbolo nel codice": False,
+        "Un elemento grafico": False}
+]
+# risposta_giusta_1 = "Central Processing Unit"
+# risposta_giusta_2 = "Random Access Memory"
+# risposta_giusta_3 = "Un'unità di memoria"
 
 # risposte_corrette = ["Central Processing Unit",
 #                      "Random Access Memory",
@@ -43,4 +40,5 @@ risposta_giusta_3 = "Un'unità di memoria"
 # mostra_domande(domande[1], risposta_2)
 # mostra_domande(domande[2], risposta_3)
 
-mostra_domande(domande[0], risp)
+for n in range(len(domande)):
+    mostra_domande(domande[n], risp[n])
