2ce1fc9f8101ec75748c200220510a7e421b8e57
mattcar <mattcar@example.com>
2025-02-05 20:11:57 +0100

    inserito conteggio finale + possibilità di ripetere

--- presenta_domande.py
+++ presenta_domande.py
@@ -1,25 +1,44 @@
 import random
+
 def mostra_domande(domanda: str, risposte: dict):
     print(f"Domanda: {domanda}")
     numero_opzione = 1
-    numero_risp_giusta = 0
+    global conta_risposte_giuste
     risposte_tuple = list(risposte.items())
     random.shuffle(risposte_tuple)
-    for option, value in risposte_tuple:
-        print(f"{numero_opzione}. {option}")
-        if value == True:
+    for opzione, verità in risposte_tuple:
+        print(f"{numero_opzione}. {opzione}")
+        if verità == True:
             numero_risp_giusta = numero_opzione
-            risposta_giusta = option
+            risposta_giusta = opzione
         numero_opzione += 1
     risp_utente = input(f"Scegli una risposta indicando il numero corrispondente: ")
     if int(risp_utente) == numero_risp_giusta:
         print("Complimenti, risposta corretta!\n")
+        conta_risposte_giuste += 1        
     else: 
-        print(f"Risposta errata. La risposta corretta era: {risposta_giusta}.\n")
+        print(f"Risposta sbagliata. La risposta corretta era: {risposta_giusta}.\n")
+
+def conteggio_finale():
+    print(f"Il quiz è finito.\nHai risposto correttamente a {conta_risposte_giuste} domande su {len(domande)}.")
+
+def ripeti_quiz():
+    risposta = input("Vuoi ripetere il quiz? S/N\n")
+    if risposta == "N".lower():
+        print("Grazie, alla prossima!")
+    else:
+        ciclo_quiz()
+
+def inizia_quiz():
+    print("Iniziamo il quiz!\n")
+    for n in range(len(domande)):
+        mostra_domande(domande[n], risposte[n])
 
 domande = ["Cos'è la CPU?",
            "Cosa significa RAM?",
-           "Cos'è un bit?"]
+           "Cos'è un bit?",
+           "Quando è uscita la prima versione di Python?",
+           "Quale di questi è un linguaggio di programmazione?"]
 
 risposte = [
         {"Central Processing Unit": True,
@@ -32,20 +51,22 @@ risposte = [
 
         {"Un'unità di memoria": True,
         "Un simbolo nel codice": False,
-        "Un elemento grafico": False}
+        "Un elemento grafico": False},
+        
+        {"1991": True,
+        "1978": False,
+        "1985": False},
+        
+        {"C++": True,
+        "D--": False,
+        "E==": False}
         ]
 
-# risposta_giusta_1 = "Central Processing Unit"
-# risposta_giusta_2 = "Random Access Memory"
-# risposta_giusta_3 = "Un'unità di memoria"
+def ciclo_quiz():
+    global conta_risposte_giuste
+    conta_risposte_giuste = 0
+    inizia_quiz()
+    conteggio_finale()
+    ripeti_quiz()
 
-# risposte_corrette = ["Central Processing Unit",
-#                      "Random Access Memory",
-#                      "Un'unità di memoria"]
-
-# mostra_domande(domande[0], risposta_1)
-# mostra_domande(domande[1], risposta_2)
-# mostra_domande(domande[2], risposta_3)
-
-for n in range(len(domande)):
-    mostra_domande(domande[n], risposte[n])
+ciclo_quiz()
